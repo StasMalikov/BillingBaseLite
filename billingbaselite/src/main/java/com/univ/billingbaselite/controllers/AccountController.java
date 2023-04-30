a5aaa01b5d6e4eaaf93098395cf4853fb76882e0
stma0121 <stanislav.malikov@netcracker.com>
2023-05-01 00:45:09 +0300

    step 1

---
+++ billingbaselite/src/main/java/com/univ/billingbaselite/controllers/AccountController.java
@@ -0,0 +1,40 @@
+package com.univ.billingbaselite.controllers;
+
+import com.univ.billingbaselite.models.dtos.AccountDTO;
+import com.univ.billingbaselite.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("billing-base-lite/v1/accounts")
+public class AccountController {
+    AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public List<AccountDTO> getAll() {
+        return accountService.getAll();
+    }
+
+    @PostMapping(produces = APPLICATION_JSON_VALUE)
+    public AccountDTO createAccount(AccountDTO accountDTO) {
+        return accountService.create(accountDTO);
+    }
+
+//    @GetMapping(produces = APPLICATION_JSON_VALUE)
+//    @RequestMapping("test")
+//    public AccountDTO test() {
+//        return accountService.test();
+//    }
+}
--- billingbaselite/src/main/java/com/univ/billingbaselite/models/entities/Account.java
+++ billingbaselite/src/main/java/com/univ/billingbaselite/models/entities/Account.java
@@ -1,4 +1,32 @@
 package com.univ.billingbaselite.models.entities;
 
+import com.univ.billingbaselite.models.dtos.AccountDTO;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ACCOUNT")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "ACCOUNT_STATUS")
+    private String accountStatus;
+
+    public Account(AccountDTO accountDTO) {
+        this.accountStatus = accountDTO.getAccountStatus();
+    }
 }
