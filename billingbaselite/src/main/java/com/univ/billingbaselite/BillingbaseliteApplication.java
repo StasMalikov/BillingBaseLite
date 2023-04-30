4ec3603eab45ed901b7d79b123add337da60dd60
stma0121 <stanislav.malikov@netcracker.com>
2023-04-30 20:52:15 +0300

    init

---
+++ billingbaselite/src/main/java/com/univ/billingbaselite/BillingbaseliteApplication.java
@@ -0,0 +1,13 @@
+package com.univ.billingbaselite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingbaseliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingbaseliteApplication.class, args);
+	}
+
+}
---
+++ billingbaselite/src/test/java/com/univ/billingbaselite/BillingbaseliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univ.billingbaselite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingbaseliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
